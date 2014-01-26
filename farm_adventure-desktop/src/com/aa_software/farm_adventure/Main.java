16f5926ec5045d9c7fc50961bb489d1420c3a6e1
Adam Cranford <acranford6@gatech.edu>
2014-01-25 21:21:31 -0500

    Added the tmx (tile map) files to the assets folder under the android folder. Also added the code to create and render to draw the basic game board to the screen. Clicks still don't work but we can now draw a tile map to the screen!

--- farm_adventure-desktop/src/com/aa_software/farm_adventure/Main.java
+++ farm_adventure-desktop/src/com/aa_software/farm_adventure/Main.java
@@ -8,9 +8,8 @@ public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "farm_adventure";
-		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.width = 640;
+		cfg.height = 1024;
 		
 		new LwjglApplication(new FarmAdventure(), cfg);
 	}
--- farm_adventure/src/com/aa_software/farm_adventure/model/FarmAdventure.java
+++ farm_adventure/src/com/aa_software/farm_adventure/model/FarmAdventure.java
@@ -9,19 +9,38 @@ import com.aa_software.farm_adventure.model.plot.AbstractPlot;
 import com.aa_software.farm_adventure.model.state.DefaultSelectionState;
 import com.aa_software.farm_adventure.model.state.ISelectionState;
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class FarmAdventure implements ApplicationListener {
 	private ISelectable selection;
 	private ISelectionState state;
+	
+	private OrthographicCamera camera;
+	private OrthogonalTiledMapRenderer renderer;
+	private TiledMap map;
+	
 	@Override
-	public void create() {		
+	public void create() {
+		Texture.setEnforcePotImages(false);
+		map = new TmxMapLoader().load("tileMap128.tmx");
+		renderer = new OrthogonalTiledMapRenderer(map);
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 640, 1024);
+		
 		this.selection = null;
 		this.state = new DefaultSelectionState();
 	}
 
 	@Override
 	public void dispose() {
-
+		map.dispose();
+		renderer.dispose();
 	}
 
 	@Override
@@ -40,6 +59,15 @@ public class FarmAdventure implements ApplicationListener {
 				state = state.update((AbstractUpgrade)selection);
 			}
 		}
+		
+		/* Draw the base map to the screen */
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		
+		camera.update();
+		
+		renderer.setView(camera);
+		renderer.render();
 	}
 
 	@Override
