ba0228ca7c784cd60f9354f0f888f02aed78accb
Will Holton <wholton@gatech.edu>
2014-03-04 04:32:46 -0500

    Revert "Merge branch 'master' of https://github.com/AA-Software/farm-adventure"
    This reverts commit 8d897de988e93fa8101b8816a1104d6bc849d29c, reversing
    changes made to 750512d74324c7c9bca5b31f5e72aa9c9de26055.

--- farm_adventure/src/com/aa_software/farm_adventure/presenter/screen/AbstractScreen.java
+++ farm_adventure/src/com/aa_software/farm_adventure/presenter/screen/AbstractScreen.java
@@ -1,6 +1,7 @@
 package com.aa_software.farm_adventure.presenter.screen;
 
 import com.aa_software.farm_adventure.presenter.FarmAdventure;
+import com.aa_software.farm_adventure.presenter.screen.farm_screen.AbstractFarmScreen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.files.FileHandle;
@@ -95,7 +96,6 @@ public abstract class AbstractScreen implements Screen {
 	@Override
 	public void resize(int width, int height) {
 			statusBarStage.setViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-
 	}
 
 	@Override
