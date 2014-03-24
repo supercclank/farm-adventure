bc000fc320b95964bd5b85d77882b2e9de406aaa
Lesly_S <les7uve@gmail.com>
2014-03-24 17:12:17 -0400

    Branch clean

--- farm_adventure/src/com/aa_software/farm_adventure/model/item/worker/DefaultWorker.java
+++
@@ -1,5 +0,0 @@
-package com.aa_software.farm_adventure.model.item.worker;
-
-public class DefaultWorker extends AbstractWorker {
-
-}
--- farm_adventure/src/com/aa_software/farm_adventure/presenter/state/AbstractSelectionState.java
+++
@@ -1,8 +0,0 @@
-package com.aa_software.farm_adventure.presenter.state;
-
-import com.aa_software.farm_adventure.model.item.AbstractItem;
-
-public abstract class AbstractSelectionState implements ISelectionState {
-	protected AbstractItem selection;
-
-}
