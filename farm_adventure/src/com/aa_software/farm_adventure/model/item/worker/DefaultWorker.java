534aa41fdbf66bc84664199cc7e9ab652355899d
Lesly_S <les7uve@gmail.com>
2014-03-31 07:44:01 -0400

    clear to update to master

--- farm_adventure/src/com/aa_software/farm_adventure/model/item/worker/DefaultWorker.java
+++
@@ -1,8 +0,0 @@
-package com.aa_software.farm_adventure.model.item.worker;
-
-import com.aa_software.farm_adventure.model.Inventory;
-import com.aa_software.farm_adventure.model.plot.Plot;
-
-public class DefaultWorker extends AbstractWorker {
-	public void update(Plot plot, Inventory inventory) {}
-}
--- farm_adventure/src/com/aa_software/farm_adventure/model/plot/Irrigation.java
+++
@@ -1,5 +0,0 @@
-package com.aa_software.farm_adventure.model.plot;
-
-public enum Irrigation {
-	TOP, BOTTOM, LEFT, RIGHT;
-}
