e31b65f06f812045b414ed6bbee74d2c95d2825c
Lesly_S <les7uve@gmail.com>
2014-04-01 13:20:01 -0400

    clear again to refresh and update with master

--- farm_adventure/src/com/aa_software/farm_adventure/model/farm/FarmType.java
+++
@@ -1,5 +0,0 @@
-package com.aa_software.farm_adventure.model.farm;
-
-public enum FarmType {
-	TUTORIAL, RAINFOREST, DESERT, SNOW
-}
--- farm_adventure/src/com/aa_software/farm_adventure/model/plot/PlotType.java
+++
@@ -1,5 +0,0 @@
-package com.aa_software.farm_adventure.model.plot;
-
-public enum PlotType {
-	GRASS, UNPLOWEDUNWATERED, UNPLOWEDWATERED, PLOWEDUNWATERED, PLOWEDWATERED, WATER, LEAVES
-}
