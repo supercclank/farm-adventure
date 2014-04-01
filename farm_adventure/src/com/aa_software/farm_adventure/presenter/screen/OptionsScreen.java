07c769c2353dfee904f800b7f9d0caf6104e5be2
Brandon <bjacobs30@gatech.edu>
2014-04-01 16:53:29 -0400

    Fully integrated with Audrey Sound

--- farm_adventure/src/com/aa_software/farm_adventure/presenter/screen/OptionsScreen.java
+++ farm_adventure/src/com/aa_software/farm_adventure/presenter/screen/OptionsScreen.java
@@ -1,12 +1,17 @@
 package com.aa_software.farm_adventure.presenter.screen;
 
+import com.aa_software.farm_adventure.model.audio.Sounds;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Slider;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.Align;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 public class OptionsScreen extends AbstractScreen {
 
+	public final Sounds sounds = Sounds.getInstance();
+	
 	private Slider masterVolume;
 	private Slider gameVolume;
 	private Slider musicVolume;
@@ -25,7 +30,6 @@ public class OptionsScreen extends AbstractScreen {
 		// Create table
 		Table table = new Table(super.getSkin());
 		table.setFillParent(true);
-		table.debug();
 		
 		// Register table
 		super.addActor(table);
@@ -39,6 +43,15 @@ public class OptionsScreen extends AbstractScreen {
 		//Create masterVolume label
 		table.add("Master Volume: ");
 		masterVolume = new Slider(0, 100, 5, false, super.getSkin());
+		
+		//Add listener
+		masterVolume.addListener(new ChangeListener(){
+			public void changed(ChangeEvent event, Actor actor) {
+				masterValue.setText("" + (int)masterVolume.getValue() + "%");
+				sounds.setMusicVolume( (masterVolume.getValue()/100) * (musicVolume.getValue()/100));
+				sounds.setSoundVolume( (masterVolume.getValue()/100) * (gameVolume.getValue()/100));
+			}
+		});
 		table.add(masterVolume);
 		masterValue = new Label("" + (int)masterVolume.getValue() + "%", super.getSkin());
 		table.add(masterValue);
@@ -49,6 +62,14 @@ public class OptionsScreen extends AbstractScreen {
 		//Create gameVolume label
 		table.add("Game Volume: ");
 		gameVolume = new Slider(0, 100, 5, false, super.getSkin());
+		
+		//Add listener
+		gameVolume.addListener(new ChangeListener(){
+			public void changed(ChangeEvent event, Actor actor) {
+				gameValue.setText("" + (int)masterVolume.getValue() + "%");
+				sounds.setSoundVolume( (masterVolume.getValue()/100) * (gameVolume.getValue()/100));
+			}
+		});
 		table.add(gameVolume);
 		gameValue = new Label("" + (int)gameVolume.getValue() + "%", super.getSkin());
 		table.add(gameValue);
@@ -57,6 +78,13 @@ public class OptionsScreen extends AbstractScreen {
 		//Create musicVolume label
 		table.add("Music Volume: ");
 		musicVolume = new Slider(0, 100, 5, false, super.getSkin());
+		//Add listener
+		musicVolume.addListener(new ChangeListener(){
+			public void changed(ChangeEvent event, Actor actor) {
+				musicValue.setText("" + (int)masterVolume.getValue() + "%");
+				sounds.setMusicVolume( (masterVolume.getValue()/100) * (musicVolume.getValue()/100));
+			}
+		});
 		table.add(musicVolume);
 		musicValue = new Label("" + (int)musicVolume.getValue() + "%", super.getSkin());
 		table.add(musicValue);
@@ -66,7 +94,6 @@ public class OptionsScreen extends AbstractScreen {
 	public void render(float delta) {
 		super.render(delta);
 		
-		masterValue.setText("" + (int)masterVolume.getValue() + "%");
 		gameValue.setText("" + (int)gameVolume.getValue() + "%");
 		musicValue.setText("" + (int)musicVolume.getValue() + "%");
 	}
